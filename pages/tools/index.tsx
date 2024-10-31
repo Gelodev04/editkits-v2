061bffcc396717b9b9c2076b49acd642c237535f
Alazar-dev <mealazartadesse@gmail.com>
2024-10-31 16:28:43 +0300

    fix: reponsiveness

--- pages/tools/index.tsx
+++ pages/tools/index.tsx
@@ -12,14 +12,15 @@ export default function Tools() {
         />
       </div>
 
-      <div style={{boxShadow: "0px 20px 12px #fafafa"}} className="border-solid border-2 border-zinc-100 py-6 rounded rounded-md justify-center mt-12 2xl:px-[1%] sm:mx-10 xl:mx-10 2xl:mx-40">
-        <Typography
-          label="Video Tools"
-          variant="h4"
-          center
-        />
-        <div className="flex justify-center">
-          <ToolsList />
+      <div className="flex justify-center">
+        <div style={{boxShadow: "0px 20px 12px #fafafa"}}
+             className="border-solid border-2 border-zinc-100 py-6 rounded rounded-md justify-center mt-12 2xl:px-[1%] 2xl:w-[70%] flex flex-col items-center">
+          <Typography
+            label="Video Tools"
+            variant="h4"
+            center
+          />
+          <ToolsList tools />
         </div>
       </div>
     </div>

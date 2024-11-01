09792d0c95da0434fc8abb4e8812dd0e4879c13c
Alazar Tadesse <69623057+Alazar-dev@users.noreply.github.com>
2024-11-01 10:12:50 +0300

    fix: updated tools image with svg format (#29)

--- lib/constants.tsx
+++ lib/constants.tsx
@@ -1,12 +1,12 @@
-import OverlayVideo from "../assets/img/tools/overlay.png"
-import Rotate from "../assets/img/tools/rotate.png"
-import Trim from "../assets/img/tools/trim.png"
-import Speed from "../assets/img/tools/speed_up_down.png"
-import Resize from "../assets/img/tools/resize.png"
-import OverlayImage from "../assets/img/tools/overlay_image.png"
-import Crop from "../assets/img/tools/crop.png"
-import Loop from "../assets/img/tools/loop.png"
-import Join from "../assets/img/tools/join.png"
+import OverlayVideo from "../assets/img/tools/overlay.svg"
+import Rotate from "../assets/img/tools/rotate.svg"
+import Trim from "../assets/img/tools/trim.svg"
+import Speed from "../assets/img/tools/speed_up_down.svg"
+import Resize from "../assets/img/tools/resize.svg"
+import OverlayImage from "../assets/img/tools/overlay_image.svg"
+import Crop from "../assets/img/tools/crop.svg"
+import Loop from "../assets/img/tools/loop.svg"
+import Join from "../assets/img/tools/join.svg"
 
 export const videoTools = [
     {name: "Overlay Video", icon: OverlayVideo},

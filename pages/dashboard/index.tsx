0065ab0c47297edf62ce97553c6a3472f2b6b782
Alazar-dev <mealazartadesse@gmail.com>
2024-11-14 11:28:38 +0300

    fix: width

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -8,17 +8,19 @@ export default function Dashboard() {
   const [active, setActive] = useState("Job status")
 
   return (
-    <div className="bg-[#fafbfc] p-6">
-      <div className="grid grid-cols-12 max-w-[1536px] mx-auto">
-        <div className="col-span-2 xl:col-span-3 2xl:col-span-2">
-          <TableType active={active} setActive={setActive}/>
-        </div>
-        <div className="col-span-9">
-          <div className="flex gap-4">
-            {stats.map(stat => <StatCard stat={stat}/>)}
+    <div className="bg-[#fafbfc]">
+      <div className="max-w-[1536px] mx-auto p-6">
+        <div className="grid grid-cols-12 w-full mx-auto">
+          <div className="col-span-2 xl:col-span-3 2xl:col-span-2">
+            <TableType active={active} setActive={setActive}/>
           </div>
-          <div className="pt-4 w-full">
-            <DashboardTable data={tableData} active={active} />
+          <div className="col-span-9">
+            <div className="flex gap-4">
+              {stats.map(stat => <StatCard stat={stat}/>)}
+            </div>
+            <div className="pt-4 w-full">
+              <DashboardTable data={tableData} active={active} />
+            </div>
           </div>
         </div>
       </div>

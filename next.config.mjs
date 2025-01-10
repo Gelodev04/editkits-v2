e264fb153b3b2c99a8ccdac8c80047461069748f
Alazar-dev <mealazartadesse@gmail.com>
2025-01-10 14:14:51 +0300

    fix: removed unnecessary configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true
-      }
-    ]
-  },
-  experimental: {
-    appDir: false
-  },
   images: {
     unoptimized: true
   },
-  distDir: 'build'
 };
 
 export default nextConfig;

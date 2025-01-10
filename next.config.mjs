082f47f7b52599c54084f6ff057ccdddf2692b6d
Alazar-dev <mealazartadesse@gmail.com>
2025-01-10 16:11:40 +0300

    fix: back to stable ssr version

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true
-  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true
+      }
+    ]
+  }
 };
 
 export default nextConfig;

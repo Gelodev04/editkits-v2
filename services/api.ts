4543331b0b5990e7cd564e9df40bfd059d412b8c
Alazar-dev <mealazartadesse@gmail.com>
2025-01-11 04:54:38 +0300

    fix: API_URL

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL }),
   endpoints: (builder) => ({
     register: builder.mutation({
       query: ({ name, email, password }) => ({

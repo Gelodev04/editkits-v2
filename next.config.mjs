d386368c39ce3016852d6fc83d9b9f8535ff702e
Alazar-dev <mealazartadesse@gmail.com>
2025-01-16 12:41:09 +0300

    fix: revert back proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://ec2-3-110-220-105.ap-south-1.compute.amazonaws.com:8081/api/v1/:path*',
-      },
-    ]
-  },
   async redirects() {
     return [
       {

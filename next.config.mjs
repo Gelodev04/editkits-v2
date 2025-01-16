f06e58d13e666e7ba9f08537f386894044e59a0e
Alazar-dev <mealazartadesse@gmail.com>
2025-01-16 12:34:50 +0300

    fix: rewrite the base url

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://ec2-3-110-220-105.ap-south-1.compute.amazonaws.com:8081/api/v1/:path*',
+      },
+    ]
+  },
   async redirects() {
     return [
       {

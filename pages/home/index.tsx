bc2a020eefc5f5fdbe89d151c544242219e024d8
Alazar-dev <mealazartadesse@gmail.com>
2025-02-26 09:25:39 +0300

    fix: features card grid

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
           <h1
             className="font-montserrat font-extrabold text-[40px] leading-[56px] tracking-[0.2px] text-transparent bg-clip-text bg-gradient-to-r from-[#637EFF] to-[#148CFC]">Edit,
             Process & Automate</h1>
-          <div className="w-[526px] pt-[3.5px] pb-[22px]">
+          <div className="w-[520px] pt-[3.5px] pb-[22px]">
             <Typography
               label="Empower your creativity with powerful yet easy-to-use toolkits and APIs on the cloud. Build custom workflows by connecting tools for seamless video, image, and audio editing."
               variant="b2"
@@ -104,7 +104,7 @@ export default function Home() {
         <Typography label="Learn more about our features" variant="h2"/>
       </div>
       <div className="bg-[url(../assets/img/inclined_bg.svg)] bg-no-repeat bg-cover">
-        <div className="grid sm:grid-cols-4 md:grid-cols-3 gap-y-[40px] pt-[72px] pb-[116px] max-w-[1536px] mx-auto">
+        <div className="grid sm:grid-cols-3 2xl:grid-cols-4 gap-y-[40px] pt-[72px] pb-[116px] max-w-[1536px] mx-auto">
           {featureCards.map((card) => <FeatureCard name={card.name} icon={card.image} description={card.description}/>)}
         </div>
       </div>

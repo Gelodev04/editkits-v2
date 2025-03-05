cdf095122edfd7d6680fdd79dad6f0b6eed850d7
Alazar-dev <mealazartadesse@gmail.com>
2025-03-05 23:33:30 +0300

    fix: blog images

--- components/cards/BlogCard/index.tsx
+++ components/cards/BlogCard/index.tsx
@@ -14,22 +14,24 @@ export default function BlogCard({img, title, date, slug}) {
   };
 
   return (
-    <Link href={`/blog/${slug}`} className="w-[433px] border-[1px] border-[bebebe] pt-[30px] px-[22px] rounded-[4px]">
-      <div className="w-[390px] h-[312px] flex justify-center items-center">
-        <Image
-          className="w-full h-full object-cover"
-          loading="lazy"
-          width={390}
-          height={312}
-          src={img}
-          alt="image"
-          unoptimized
-        />
-      </div>
-      <p className="font-montserrat font-bold text-[18.2px] leading-[21.84px] text-[#2c2c2c] pt-[21px]">{title}</p>
-      <div className="flex pt-[6.3px] pb-[34.16px] gap-[4px] items-center">
-        <Image loading="lazy" src={CalendarIcon} alt="icon" />
-        <p className="font-lato font-normal text-base leading-[28px] text-[#A0AEC0]">{formatDate(new Date(date))}</p>
+    <Link href={`/blog/${slug}`}>
+      <div className="w-[433px] border-[1px] border-gray-300 border-opacity-65 pt-[30px] px-[22px] rounded-[4px] max-h-[478px]">
+        <div className="w-[390px] h-[312px] flex justify-center items-center">
+          <Image
+            className="w-full h-full object-cover"
+            loading="lazy"
+            width={390}
+            height={312}
+            src={img}
+            alt="image"
+            unoptimized
+          />
+        </div>
+        <p className="font-montserrat font-bold text-[18.2px] leading-[21.84px] text-[#2c2c2c] pt-[21px]">{title}</p>
+        <div className="flex pt-[6.3px] pb-[34.16px] gap-[4px] items-center">
+          <Image loading="lazy" src={CalendarIcon} alt="icon" />
+          <p className="font-lato font-normal text-base leading-[28px] text-[#A0AEC0]">{formatDate(new Date(date))}</p>
+        </div>
       </div>
     </Link>
   )

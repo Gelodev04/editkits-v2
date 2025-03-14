9f48ade0ca11b230bad4f3f869e21bb1b825cdf9
Alazar-dev <mealazartadesse@gmail.com>
2025-03-14 11:14:11 +0300

    search functionality

--- components/ToolsList/index.tsx
+++ components/ToolsList/index.tsx
@@ -1,12 +1,12 @@
 import {videoTools} from "@/lib/constants";
 import ToolCard from "../cards/ToolCard/index";
 
-export default function ToolsList() {
+export default function ToolsList({filterBy}) {
   return (
     <>
       <div className="sm:block xl:hidden place-items-center">
         <div className={`place-items-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-y-10 gap-x-6 py-10 px-6 w-full`}>
-          {videoTools.map((tool) => (
+          {videoTools.filter(tool => tool.name.toLowerCase().includes(filterBy)).map((tool) => (
             <ToolCard
               key={tool.name}
               name={tool.name}
@@ -18,7 +18,7 @@ export default function ToolsList() {
       </div>
       <div className="sm:hidden xl:block place-items-center">
         <div className={`place-items-center grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-y-10 gap-x-6 py-10 px-6 w-full`}>
-          {videoTools.slice(0, 5).map((tool) => (
+          {videoTools.slice(0, 5).filter(tool => tool.name.toLowerCase().includes(filterBy)).map((tool) => (
             <ToolCard
               key={tool.name}
               name={tool.name}
@@ -28,7 +28,7 @@ export default function ToolsList() {
           ))}
         </div>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10 gap-x-4 pb-6 px-6">
-          {videoTools.slice(5).map((tool) => (
+          {videoTools.slice(5).filter(tool => tool.name.toLowerCase().includes(filterBy)).map((tool) => (
             <ToolCard
               key={tool.name}
               name={tool.name}
--- pages/tools/index.tsx
+++ pages/tools/index.tsx
@@ -1,20 +1,36 @@
+import React from 'react';
+
 import ToolsList from "../../components/ToolsList/index";
+import Typography from "@/components/Typography";
+import Search from "@/components/Search";
 
 export default function Tools() {
-  return (
-    <div className="mx-8">
-      <div className="mb-20 max-w-[1920px] mx-auto">
-        <div className="pt-16">
-          <p className="font-montserrat font-extrabold text-[36px] text-[#2c2c2c] leading-[45px] text-center">Tools</p>
-        </div>
+  const [filterBy, setFilterBy] = React.useState("");
 
-        <div
-          style={{boxShadow: "0px 20px 12px #fafafa"}}
-           className="border-solid border-2 border-zinc-100 py-6 rounded rounded-md justify-center mt-12 flex flex-col items-center max-w-[1920px] mx-auto"
-        >
-          <p className="font-lato font-bold text-[32px] leading-[48px] text-[#4f4f4f]">Video Tools</p>
-          <ToolsList />
-        </div>
+  return (
+    <div className="pt-[56px] max-w-[1200px] mx-auto">
+      <Typography variant="h1" label="Tools" center />
+      <div className="pt-[12px] pb-[40px]">
+        <Typography
+          variant="b2"
+          label="Empower your creativity with a suite of powerful but easy to use toolkits for video, image and audio editing."
+          center
+        />
+      </div>
+      <div className="max-w-[567px] mx-auto">
+        <Search
+          setFilterBy={setFilterBy}
+        />
+      </div>
+
+      <div
+        style={{boxShadow: "0px 20px 12px #fafafa"}}
+        className="border-solid border-2 border-zinc-100 py-6 rounded rounded-md justify-center mt-12 flex flex-col items-center max-w-[1920px] mx-auto"
+      >
+        <p className="font-lato font-bold text-[32px] leading-[48px] text-[#4f4f4f]">Video Tools</p>
+        <ToolsList
+          filterBy={filterBy}
+        />
       </div>
     </div>
   )

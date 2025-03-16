f8c79e4980ee88d548ad848e069ec04cb1402da7
Alazar-dev <mealazartadesse@gmail.com>
2025-03-16 10:54:15 +0300

    fix request header

--- services/api/index.ts
+++ services/api/index.ts
@@ -254,13 +254,7 @@ export const api = createApi({
       query: ({slug}) => {
         return {
           url: `/blog/entry?slug=${slug}`,
-          method: 'GET',
-          headers: {
-            "Content-Type": "application/json",
-            Referer: "https://www.editkits.com/",
-            Origin: "https://www.editkits.com/",
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
-          }
+          method: 'GET'
         }
       },
       transformResponse: (response) => {

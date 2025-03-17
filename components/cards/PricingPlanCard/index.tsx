ba44368404ff37a01ecafb57bb388d91116839bd
Alazar-dev <mealazartadesse@gmail.com>
2025-03-17 11:24:53 +0300

    ref: fetch pricing data

--- components/cards/PricingPlanCard/index.tsx
+++ components/cards/PricingPlanCard/index.tsx
@@ -4,20 +4,19 @@ import BenefitCard from "@/components/cards/BenefitCard";
 
 type PricingPlanCardProps = {
   title: string;
-  credits: string;
+  credits: number;
   description: string;
-  originalPrice: string | number;
-  type: string;
+  original_price: string | number;
   benefits: string[];
-  discountPrice?: number;
-  mostPopular: boolean;
+  new_price?: number;
+  is_popular: boolean;
 }
 
 export default function PricingPlanCard(props: PricingPlanCardProps) {
   return (
     <div
       className="flex relative flex-col justify-between bg-white pt-[44px] pb-[25px] pl-[32px] border border-solid border-[#5243C2] group hover:text-white hover:bg-[#7D3DDE] w-[354px] rounded-2xl min-h-[552px] transition-all duration-300 shadow-lg">
-      {props.mostPopular && props.mostPopular && (
+      {props.is_popular && props.is_popular && (
         <div
           className="absolute top-[-12px] left-1/2 transform -translate-x-1/2 bg-[#273266] text-white text-[10px] font-montserrat font-semibold  leading-[12.19px] pt-[7px] pb-[8px] pl-[15.76px] pr-[16.97px] w-[146.3px] tracking-[0.8px] text-center rounded-full">
           MOST POPULAR
@@ -27,11 +26,11 @@ export default function PricingPlanCard(props: PricingPlanCardProps) {
         <div className="flex justify-between items-center pr-[15px]">
           <div className="flex items-center">
             <p
-              className="font-montserrat font-semibold text-[36px] leading-[46px] text-[#2c2c2c] group-hover:text-white">{"$" + props.originalPrice}</p>
+              className="font-montserrat font-semibold text-[36px] leading-[46px] text-[#2c2c2c] group-hover:text-white">{"$" + props.original_price}</p>
             <p
               className="italic font-lato font-medium text-[17px] leading-[20.4px] text-[#2c2c2c] pl-[6px] group-hover:text-white">/month</p>
           </div>
-          <Tag label={props.credits}/>
+          <Tag label={`${props.credits} / month`}/>
         </div>
         <p
           className="font-montserrat font-semibold text-[28px] leading-[34.13px] text-[#2c2c2c] pt-[20px] pb-[10px] group-hover:text-white">{props.title}</p>

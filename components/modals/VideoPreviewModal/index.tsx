9ac4df83cbc3fbe474671f818a31b8cd824a81da
Alazar-dev <mealazartadesse@gmail.com>
2025-03-22 19:40:45 +0300

    wip: video size

--- components/modals/VideoPreviewModal/index.tsx
+++ components/modals/VideoPreviewModal/index.tsx
@@ -1,7 +1,7 @@
-import React, { useRef, useEffect, SetStateAction } from "react";
-import { Fade, Modal } from "@mui/material";
+import React, {useRef, useEffect, SetStateAction} from "react";
+import {Fade, Modal} from "@mui/material";
 
-import { lato, montserrat, opensans } from "@/lib/fonts";
+import {lato, montserrat, opensans} from "@/lib/fonts";
 import "video.js/dist/video-js.css";
 
 type PopUpProps = {
@@ -18,18 +18,16 @@ export default function VideoPreviewModal(props: PopUpProps) {
       <Fade in={props.open}>
         {/* @ts-ignore */}
         <div>
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true" />
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"/>
           <div
             className="fixed inset-0 z-10 w-screen overflow-y-auto flex min-h-full items-end justify-center text-center sm:items-center sm:p-0"
           >
             <div
-              className={`${montserrat.variable} ${lato.variable} ${opensans.variable} relative transform overflow-hidden rounded-3xl bg-white text-left shadow-xl transition-all sm:w-[532px]`}
+              className={`${montserrat.variable} ${lato.variable} ${opensans.variable} relative transform overflow-hidden rounded-3xl bg-white text-left shadow-xl transition-all w-[532px]`}
             >
-              <div>
-                <video ref={videoNode} className="video-js vjs-default-skin" controls>
-                  <source src={props.videoUrl?.url} type="video/mp4" />
-                </video>
-              </div>
+              <video ref={videoNode} className="video-js vjs-default-skin w-[532px]" controls>
+                <source src={props.videoUrl?.url} type="video/mp4"/>
+              </video>
             </div>
           </div>
         </div>

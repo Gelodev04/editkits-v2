282f0b829df78b6e751cad80320bc7687be09227
Alazar-dev <mealazartadesse@gmail.com>
2025-04-01 10:29:40 +0300

    new stat card

--- components/cards/StatCard/index.tsx
+++ components/cards/StatCard/index.tsx
@@ -1,45 +1,36 @@
-import Image from 'next/image'
-import { ArrowUpIcon } from "@/icons";
-import Badge from "@/components/Badge";
-
 type StatData = {
-  title: string;
-  value: string;
-  icon: any
+  label: string;
+  data: any;
 }
 
 export default function StatCard(props: any) {
   return (
     <>
-      {/*@ts-ignore*/}
-      {props.stat.map(((s: StatData) => <Stat title={s.label} value={s.data[0].value} icon={s.icon}/>))}
-    </>
-  )
-}
-
-
-function Stat({title, value, icon}: { title: string; value: string , icon: any}) {
-  return (
-    <div
-      className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
-      <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
-        <Image className="text-gray-800 size-6 dark:text-white/90" src={icon} alt="group icon"/>
-      </div>
-
-      <div className="flex items-end justify-between mt-5">
-        <div>
-            <span className="text-sm text-gray-500 dark:text-gray-400">
-              {title}
-            </span>
-          <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
-            {value}
-          </h4>
+      <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03]">
+        <div className="mb-8 flex flex-col justify-between gap-4 sm:flex-row sm:items-center">
+          <div>
+            <h3 className="text-lg font-semibold text-gray-800 dark:text-white">
+              Overview
+            </h3>
+          </div>
         </div>
-        <Badge color="success">
-          <Image src={ArrowUpIcon} alt="up icon"/>
-          11.01%
-        </Badge>
+        <div
+          className="grid rounded-2xl border border-gray-200 bg-white sm:grid-cols-2 xl:grid-cols-2 dark:border-gray-800 dark:bg-gray-900">
+          {props.stat.map(((s: StatData) => (
+
+            <div className="border-b border-gray-200 px-6 py-5 sm:border-r xl:border-b-0 dark:border-gray-800">
+              <span className="text-sm text-gray-500 dark:text-gray-400">
+                {s.label}
+              </span>
+              <div className="mt-2 flex items-end gap-3">
+                <h4 className="text-title-xs sm:text-title-sm font-bold text-gray-800 dark:text-white/90">
+                  {s.data[0].value}
+                </h4>
+              </div>
+            </div>
+          )))}
+        </div>
       </div>
-    </div>
+    </>
   )
 }

426c7f878984ba11be5485b85389835ad7ce64f8
Alazar-dev <mealazartadesse@gmail.com>
2025-04-02 19:14:15 +0300

    fix: pagination theme

--- pages/dashboard/uploaded-files/index.tsx
+++ pages/dashboard/uploaded-files/index.tsx
@@ -99,30 +99,30 @@ export default function JobStatus() {
 
   return (
     <div className="flex justify-center">
-
       <div
-        className={`${mainContentMargin} min-h-screen mt-10 transition-all duration-300 ease-in-out overflow-hidden bg-white dark:bg-white/[0.03] rounded-xl sm:w-[1280px] sm:mx-10 lg:mx-0 lg:w-[1536px] p-6`}>
-        <div
-          className="flex flex-col gap-2 px-4 py-4 border border-b-0 border-gray-100 dark:border-white/[0.05] rounded-t-xl sm:flex-row sm:items-center sm:justify-between">
-          <div className="flex items-center gap-3">
-            <span className="text-gray-500 dark:text-gray-400"> Show </span>
-            <div className="relative z-20 bg-transparent">
-              <select
-                className="w-full py-2 pl-3 pr-8 text-sm text-gray-800 bg-transparent border border-gray-300 rounded-lg appearance-none dark:bg-dark-900 h-9 bg-none shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
-                value={itemsPerPage}
-                onChange={(e) => setItemsPerPage(Number(e.target.value))}
-              >
-                {[5, 8, 10].map((value) => (
-                  <option
-                    key={value}
-                    value={value}
-                    className="text-gray-500 dark:bg-gray-900 dark:text-gray-400"
-                  >
-                    {value}
-                  </option>
-                ))}
-              </select>
-              <span className="absolute z-30 text-gray-500 -translate-y-1/2 right-2 top-1/2 dark:text-gray-400">
+        className={`${mainContentMargin} min-h-screen mt-10 transition-all duration-300 ease-in-out overflow-hidden dark:bg-white/[0.03] rounded-xl sm:w-[1280px] sm:mx-10 lg:mx-0 lg:w-[1536px] p-6`}>
+        <div className="bg-white dark:bg-white/[0.03] rounded-xl p-5">
+          <div
+            className="flex flex-col gap-2 px-4 py-4 border border-b-0 border-gray-100 dark:border-white/[0.05] rounded-t-xl sm:flex-row sm:items-center sm:justify-between">
+            <div className="flex items-center gap-3">
+              <span className="text-gray-500 dark:text-gray-400"> Show </span>
+              <div className="relative z-20 bg-transparent">
+                <select
+                  className="w-full py-2 pl-3 pr-8 text-sm text-gray-800 bg-transparent border border-gray-300 rounded-lg appearance-none dark:bg-dark-900 h-9 bg-none shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
+                  value={itemsPerPage}
+                  onChange={(e) => setItemsPerPage(Number(e.target.value))}
+                >
+                  {[5, 8, 10].map((value) => (
+                    <option
+                      key={value}
+                      value={value}
+                      className="text-gray-500 dark:bg-gray-900 dark:text-gray-400"
+                    >
+                      {value}
+                    </option>
+                  ))}
+                </select>
+                <span className="absolute z-30 text-gray-500 -translate-y-1/2 right-2 top-1/2 dark:text-gray-400">
                   <svg
                     className="stroke-current"
                     width="16"
@@ -140,125 +140,128 @@ export default function JobStatus() {
                     />
                   </svg>
                 </span>
+              </div>
+              <span className="text-gray-500 dark:text-gray-400"> entries </span>
             </div>
-            <span className="text-gray-500 dark:text-gray-400"> entries </span>
           </div>
-        </div>
-        <div className="overflow-x-auto custom-scrollbar">
-          <div>
-            <Table>
-              <TableHeader className="border-t border-gray-100 dark:border-white/[0.05]">
-                <TableRow>
-                  {uploadedFilesColumns?.map(({name}) => (
-                    <TableCell
-                      key={name}
-                      isHeader
-                      className="px-4 py-3 border border-gray-100 dark:border-white/[0.05]"
-                    >
-                      <div
-                        className="flex items-center justify-between cursor-pointer"
-                        onClick={() => handleSort(name as SortKey)}
+          <div className="overflow-x-auto custom-scrollbar">
+            <div>
+              <Table>
+                <TableHeader className="border-t border-gray-100 dark:border-white/[0.05]">
+                  <TableRow>
+                    {uploadedFilesColumns?.map(({name}) => (
+                      <TableCell
+                        key={name}
+                        isHeader
+                        className="px-4 py-3 border border-gray-100 dark:border-white/[0.05]"
                       >
-                        <p className="font-medium text-gray-700 text-theme-xs dark:text-gray-400">
-                          {name}
-                        </p>
-                        <button className={!name ? "hidden" : "flex flex-col gap-0.5 text-gray-800 dark:text-gray-700"}>
-                          <svg
-                            width="8"
-                            height="5"
-                            viewBox="0 0 8 5"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M4.40962 4.41483C4.21057 4.69919 3.78943 4.69919 3.59038 4.41483L1.05071 0.786732C0.81874 0.455343 1.05582 0 1.46033 0H6.53967C6.94418 0 7.18126 0.455342 6.94929 0.786731L4.40962 4.41483Z"
-                              fill="currentColor"
-                            />
-                          </svg>
-                          <svg
+                        <div
+                          className="flex items-center justify-between cursor-pointer"
+                          onClick={() => handleSort(name as SortKey)}
+                        >
+                          <p className="font-medium text-gray-700 text-theme-xs dark:text-gray-400">
+                            {name}
+                          </p>
+                          <button
+                            className={!name ? "hidden" : "flex flex-col gap-0.5 text-gray-800 dark:text-gray-700"}>
+                            <svg
+                              width="8"
+                              height="5"
+                              viewBox="0 0 8 5"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M4.40962 4.41483C4.21057 4.69919 3.78943 4.69919 3.59038 4.41483L1.05071 0.786732C0.81874 0.455343 1.05582 0 1.46033 0H6.53967C6.94418 0 7.18126 0.455342 6.94929 0.786731L4.40962 4.41483Z"
+                                fill="currentColor"
+                              />
+                            </svg>
+                            <svg
 
-                            width="8"
-                            height="5"
-                            viewBox="0 0 8 5"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M4.40962 0.585167C4.21057 0.300808 3.78943 0.300807 3.59038 0.585166L1.05071 4.21327C0.81874 4.54466 1.05582 5 1.46033 5H6.53967C6.94418 5 7.18126 4.54466 6.94929 4.21327L4.40962 0.585167Z"
-                              fill="currentColor"
-                            />
-                          </svg>
-                        </button>
-                      </div>
-                    </TableCell>
-                  ))}
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {currentData?.map((job, i) => (
-                  <TableRow key={i + 1}>
-                    <TableCell className="px-4 py-3 border border-gray-100 dark:border-white/[0.05] whitespace-nowrap">
-                      <div className="flex items-center gap-3">
-                        <div className="w-10 h-10 overflow-hidden rounded-full">
-                          <Image
-                            width={40}
-                            height={40}
-                            src={job.thumbnail_url === "EXPIRED" ? ExpiredIcon : job.thumbnail_url}
-                            alt="user"
-                          />
+                              width="8"
+                              height="5"
+                              viewBox="0 0 8 5"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M4.40962 0.585167C4.21057 0.300808 3.78943 0.300807 3.59038 0.585166L1.05071 4.21327C0.81874 4.54466 1.05582 5 1.46033 5H6.53967C6.94418 5 7.18126 4.54466 6.94929 4.21327L4.40962 0.585167Z"
+                                fill="currentColor"
+                              />
+                            </svg>
+                          </button>
                         </div>
-                      </div>
-                    </TableCell>
-                    <TableCell
-                      className="px-4 py-3 font-normal dark:text-gray-400/90 text-gray-800 border border-gray-100 dark:border-white/[0.05] text-theme-sm whitespace-nowrap">
-                      {job.input_file_id}
-                    </TableCell>
-                    <TableCell
-                      className="px-4 py-3 font-normal dark:text-gray-400/90 text-gray-800 border border-gray-100 dark:border-white/[0.05] text-theme-sm whitespace-nowrap">
-                      {job.input_file_name}
-                    </TableCell>
-                    <TableCell
-                      className="px-4 py-3 font-normal dark:text-gray-400/90 text-gray-800 border border-gray-100 dark:border-white/[0.05] text-theme-sm whitespace-nowrap">
-                      {new Date(job.created_at * 1000).toLocaleDateString('en-GB') + " " + new Date(job.created_at * 1000).toLocaleTimeString('en-GB')}
-                    </TableCell>
-                    <TableCell
-                      className="px-4 py-3 font-normal dark:text-gray-400/90 text-gray-800 border border-gray-100 dark:border-white/[0.05] text-theme-sm whitespace-nowrap">
-                      {job.tools_used}
-                    </TableCell>
-                    <TableCell
-                      className="text-center px-4 py-3 font-normal dark:text-gray-400/90 text-gray-800 border border-gray-100 dark:border-white/[0.05] text-theme-sm whitespace-nowrap">
-                      {job.credits}
-                    </TableCell>
-                    <TableCell
-                      className="text-center px-4 py-3 font-normal dark:text-gray-400/90 text-gray-800 border border-gray-100 dark:border-white/[0.05] text-theme-sm whitespace-nowrap">
-                      <Badge
-                        color={job.status === "SUCCESS" ? "success" : job.status === "IN_PROGRESS" ? "info" : "warning"}
-                      >
-                        {statusMapper(job.status)}
-                      </Badge>
-                    </TableCell>
-                    <TableCell
-                      className="text-center px-4 py-3 font-normal dark:text-gray-400/90 text-gray-800 border border-gray-100 dark:border-white/[0.05] text-theme-sm whitespace-nowrap">
-                      {job.is_multi_output ? job.output_file_ids?.map(id => (
-                          <div className="flex items-center gap-[6.75px]">
-                            <p
-                              className="font-lato text-sm font-normal text-[#4f4f4f] leading-[19.6px]">#{id.slice(0, 5)}...</p>
-                          </div>
-                        ))
-                        : (
-                          <p
-                            className="font-lato text-sm font-normal text-[#4f4f4f] leading-[19.6px]">#{job.output_file_id.slice(0, 5)}...</p>
-                        )}
-                    </TableCell>
+                      </TableCell>
+                    ))}
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
+                </TableHeader>
+                <TableBody>
+                  {currentData?.map((job, i) => (
+                    <TableRow key={i + 1}>
+                      <TableCell
+                        className="px-4 py-3 border border-gray-100 dark:border-white/[0.05] whitespace-nowrap">
+                        <div className="flex items-center gap-3">
+                          <div className="w-10 h-10 overflow-hidden rounded-full">
+                            <Image
+                              width={40}
+                              height={40}
+                              src={job.thumbnail_url === "EXPIRED" ? ExpiredIcon : job.thumbnail_url}
+                              alt="user"
+                            />
+                          </div>
+                        </div>
+                      </TableCell>
+                      <TableCell
+                        className="px-4 py-3 font-normal dark:text-gray-400/90 text-gray-800 border border-gray-100 dark:border-white/[0.05] text-theme-sm whitespace-nowrap">
+                        {job.input_file_id}
+                      </TableCell>
+                      <TableCell
+                        className="px-4 py-3 font-normal dark:text-gray-400/90 text-gray-800 border border-gray-100 dark:border-white/[0.05] text-theme-sm whitespace-nowrap">
+                        {job.input_file_name}
+                      </TableCell>
+                      <TableCell
+                        className="px-4 py-3 font-normal dark:text-gray-400/90 text-gray-800 border border-gray-100 dark:border-white/[0.05] text-theme-sm whitespace-nowrap">
+                        {new Date(job.created_at * 1000).toLocaleDateString('en-GB') + " " + new Date(job.created_at * 1000).toLocaleTimeString('en-GB')}
+                      </TableCell>
+                      <TableCell
+                        className="px-4 py-3 font-normal dark:text-gray-400/90 text-gray-800 border border-gray-100 dark:border-white/[0.05] text-theme-sm whitespace-nowrap">
+                        {job.tools_used}
+                      </TableCell>
+                      <TableCell
+                        className="text-center px-4 py-3 font-normal dark:text-gray-400/90 text-gray-800 border border-gray-100 dark:border-white/[0.05] text-theme-sm whitespace-nowrap">
+                        {job.credits}
+                      </TableCell>
+                      <TableCell
+                        className="text-center px-4 py-3 font-normal dark:text-gray-400/90 text-gray-800 border border-gray-100 dark:border-white/[0.05] text-theme-sm whitespace-nowrap">
+                        <Badge
+                          color={job.status === "SUCCESS" ? "success" : job.status === "IN_PROGRESS" ? "info" : "warning"}
+                        >
+                          {statusMapper(job.status)}
+                        </Badge>
+                      </TableCell>
+                      <TableCell
+                        className="text-center px-4 py-3 font-normal dark:text-gray-400/90 text-gray-800 border border-gray-100 dark:border-white/[0.05] text-theme-sm whitespace-nowrap">
+                        {job.is_multi_output ? job.output_file_ids?.map(id => (
+                            <div className="flex items-center gap-[6.75px]">
+                              <p
+                                className="font-lato text-sm font-normal text-[#4f4f4f] leading-[19.6px]">#{id.slice(0, 5)}...</p>
+                            </div>
+                          ))
+                          : (
+                            <p
+                              className="font-lato text-sm font-normal text-[#4f4f4f] leading-[19.6px]">#{job.output_file_id.slice(0, 5)}...</p>
+                          )}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
           </div>
-        </div>
 
-        <div className="border border-t-0 rounded-b-xl border-gray-100 py-4 pl-[18px] pr-4 dark:border-white/[0.05]">
+        </div>
+        <div
+          className="border bg-white dark:bg-white/[0.03] mt-5 p-2 rounded-xl border-t-0 rounded-b-xl border-gray-100 py-4 pl-[18px] pr-4 dark:border-white/[0.05]">
           <div className="flex flex-col xl:flex-row xl:items-center xl:justify-between">
-            {/* Left side: Showing entries */}
             <div className="pb-3 xl:pb-0">
               <p
                 className="pb-3 text-sm font-medium text-center text-gray-500 border-b border-gray-100 dark:border-gray-800 dark:text-gray-400 xl:border-b-0 xl:pb-0 xl:text-left">

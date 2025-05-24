c1508789cfd3e885ff86a967b5d1e0a73f0213ee
gelo <gelodev83@gmail.com>
2025-05-24 23:24:43 +0800

    updated the padding in forgot password

--- components/PasswordValidtion/index.tsx
+++ components/PasswordValidtion/index.tsx
@@ -5,7 +5,7 @@ import Typography from '@/components/Typography';
 export function PasswordValidation(props) {
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-y-[8px]">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-y-[8px] gap-x-[12px]">
         <div className="flex items-center gap-x-[6px]">
           <FaCheckCircle size={20} color={/[a-z]/.test(props.password) ? '#1d2939' : '#d9d9d9'} />
           <Typography variant="b4" label="One lowercase character" />
